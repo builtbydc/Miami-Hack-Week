d60b6427e1e2ebf19e2843918b98544e56dbefcf
Daniel Carlson <61763891+builtbydc@users.noreply.github.com>
2022-01-24 14:59:20 -0500

    add pwa

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
 function toggleNav() {
     if (document.getElementById("nav-menu").classList.contains("hidden"))
         document.getElementById("nav-menu").classList.remove("hidden");
     else
         document.getElementById("nav-menu").classList.add("hidden");
+}
+
+function registerServiceWorker() {
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./sw.js')
+            .then(function(registration) {
+                // Registration was successful
+                console.log('ServiceWorker registration successful with scope: ', registration.scope);
+            }).catch(function(err) {
+                // registration failed :(
+                console.log('ServiceWorker registration failed: ', err);
+            });
+    }
+
 }
